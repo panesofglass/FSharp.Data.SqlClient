e6b043235c8ad3637180c295e19db88395b21478
Sam Hanes <samh@olo.com>
2018-10-30 16:17:45 -0400

    netcore uom and enums work

--- src/SqlClient.Tests/SqlClient.Tests.NetCoreApp/Program.fs
+++ src/SqlClient.Tests/SqlClient.Tests.NetCoreApp/Program.fs
@@ -3,11 +3,18 @@
 [<Literal>]
 let cnx = "Data Source=.;Initial Catalog=AdventureWorks2012;Integrated Security=True"
 
-type TinyIntMapping = SqlEnumProvider<"SELECT * FROM (VALUES(('One'), CAST(1 AS tinyint)), ('Two', CAST(2 AS tinyint))) AS T(Tag, Value)", cnx>
+type TinyIntMapping = SqlEnumProvider<"SELECT * FROM (VALUES(('One'), CAST(1 AS tinyint)), ('Two', CAST(2 AS tinyint))) AS T(Tag, Value)", cnx, Kind = SqlEnumKind.CLI>
+type CurrencyCode = 
+   SqlEnumProvider<"
+       SELECT CurrencyCode
+       FROM Sales.Currency 
+       WHERE CurrencyCode IN ('USD', 'EUR', 'GBP')
+   ", cnx, Kind = SqlEnumKind.UnitsOfMeasure>
 
 [<EntryPoint>]
 let main _ =
     let get42 = new SqlCommandProvider<"SELECT 42", "Server=.;Integrated Security=True">("Server=.;Integrated Security=True")
     get42.Execute() |> Seq.toArray |> printfn "%A"
-    printfn "generative type: %A" TinyIntMapping.Items
+    printfn "uom: %A" 1m<CurrencyCode.USD>
+    printfn "cli enum: %A" TinyIntMapping.One
     0
